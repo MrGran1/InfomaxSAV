07003221e5e2e8daade0de374c091dce71eea5db
Tr0st0 <theophile.marjolet@gmail.com>
2023-07-10 01:03:56 +0200

    rien de special

--- code/infomax/infomax/settings.py
+++ code/infomax/infomax/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +121,7 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'code/infomax/static/css/style_bd.css' (BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
