720e34e2c506cefb22a01c747463fb262b64e564
theophile <theophile.marjolet@gmail.com>
2023-12-13 20:59:24 +0100

    pdf interne commencé

--- code/infomax/infomax/settings.py
+++ code/infomax/infomax/settings.py
@@ -126,7 +126,8 @@ STATIC_ROOT = os.path.join (BASE_DIR,
                             'code/infomax/static/admin/css/style_bd.css'
                             'code/infomax/static/admin/css/haut_de_page.css'
                             'code/infomax/static/admin/css/recherche_depot.css'
-                            'code/infomax/static/admin/css/login.css')
+                            'code/infomax/static/admin/css/login.css'
+                            'code/infomax/static/admin/img/logosimple.png')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
